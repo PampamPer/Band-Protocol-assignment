4f6eadbfd37c8c1e94d0a413097480b9684547e0
PampamPer <pam.45012@gmail.com>
2024-03-27 23:35:14 +0700

    second assignment

---
+++ Superman.js
@@ -0,0 +1,10 @@
+let input = [[[2, 5, 10, 12, 15], 5], [[1, 11, 30, 34, 35, 37], 10]]
+
+for(i of input) {
+    console.log(isRescue(i))
+    console.log("---------------")
+}
+
+function isRescue(arr) {
+    
+}
