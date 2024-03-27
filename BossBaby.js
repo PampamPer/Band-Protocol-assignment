9edfabcd892d6ef99091a716016bc6e8e4acbb3e
PampamPer <pam.45012@gmail.com>
2024-03-27 22:00:49 +0700

    edit file

--- BossBaby.js
+++ BossBaby.js
@@ -7,30 +7,34 @@ for(i of input){
 
 function isGoodBoy(str) {
     console.log(str)
-    let s=0, r=0, idx=0; //s is amount of shot, r is amount of revenge, idx is index of String
-    if (str.charAt(0)=="R" || str.charAt(str.length-1)=="S") return "Bad boy" //if Boss baby shot first or he didn't revenge in the last shot, he's a Bad boy.
+    // s is amount of shot, r is amount of revenge, idx is index of String
+    let s=0, r=0, idx=0; 
+
+    // if Boss baby shot first or he didn't revenge in the last shot, he's a Bad boy.
+    if (str.charAt(0)=="R" || str.charAt(str.length-1)=="S") return "Bad boy" 
     else {
-        for (i of str) { //loop for each characters in str (each actions)
-            console.log(`----- round ${idx+1} -----`)
-            // console.log("idx", idx)
-            if (i=="S") { // if current action is Boss baby got shot 
-                s++ //increase s (Boss baby got shot s time(s))
-                console.log("s", s)
+        // loop for each characters in str (each actions)
+        for (i of str) { 
+            // if current action is Boss baby got shot 
+            if (i=="S") { 
+                s++ // increase s (Boss baby got shot s time(s))
             }
-            else if (str.charAt(idx-1)=="S" || r>0){ // if previous action is Boss baby got shot or Boss baby had revenged at least 1 time,
+            
+            // if previous action is Boss baby got shot or Boss baby had revenged at least 1 time,
+            else if (str.charAt(idx-1)=="S" || r>0){ 
                 r++ // increase r (Boss baby had revenged r time(s))
-                if(s<=r && str.charAt(idx+1)=="S") { // if Boss baby had revenged more than he got shot and next action he got shot, 
-                    // console.log("next char", str.charAt(idx+1))
-                    console.log("r", r)
-                    console.log("reset")
-                    // we'll re-consider the following actions as if he didn't get shot and had revenged before
+                
+                // if Boss baby had revenged more than he got shot and next action he got shot, 
+                // we'll re-consider the following actions as if he didn't get shot and had revenged before
+                if(s<=r && str.charAt(idx+1)=="S") { 
                     s=0
                     r=0
                 }
-                console.log("r", r)
             }
             idx++ // add idx to check previous and next action in str
         }
-    return (r>=s ? "Good boy" : "Bad boy") // if Boss baby had revenged more than he got shot, he's a Good boy.
+    
+        // if Boss baby had revenged more than he got shot, he's a Good boy.
+        return (r>=s ? "Good boy" : "Bad boy") 
     }
 }
